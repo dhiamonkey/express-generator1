deb841016341cb563543f5d9c22ec900330a7193
dhia muhammad <dhiamuhammad@gmail.com>
2022-07-07 18:24:55 +0700

    test dari avi

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
+let counter = 0;
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Jon Nodemon" });
+  res.render("index", { title: "Jon Counterz", counter: counter });
+});
+
+/* POST increase counter by 1. */
+router.post("/increase-counter", function (req, res, next) {
+  counter = counter + 1;
+  res.redirect(303, "/");
 });
 
 module.exports = router;
---
+++ routes/soal.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var router = express.Router();
+
+let benar = 0;
+let salah = 0;
+
+const question = {
+  text: "Siapa yang paling tampan?",
+  options: ["Rauhan", "Miftah", "Arief", "Jon"],
+  answer: "Jon",
+};
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.render("soal", { question, benar, salah });
+});
+
+router.post("/answering", function (req, res, next) {
+  const { ans } = req.body;
+  ans === question.answer ? benar++ : salah++;
+  res.redirect("/soal");
+});
+
+module.exports = router;
